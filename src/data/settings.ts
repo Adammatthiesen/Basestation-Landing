4857ee7b999c28a6bb95021ae4582b9740b949ac
Adammatthiesen <30383579+Adammatthiesen@users.noreply.github.com>
2023-06-26 04:21:38 -0700

    Initial commit

---
+++ src/data/settings.ts
@@ -0,0 +1,7 @@
+export const settings = {
+  site: 'https://basestation.space',
+  name: 'Basestation',
+  title: 'Basestation Private Server',
+  description:
+    'This is a service provided by Basestation for Authorized Basestation Users.',
+};
